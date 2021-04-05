60c61e8faf3c33cacbb3e1b73f72c14ba9ca4d19
GOGOPOWERRANGERFYF <392138779@qq.com>
2021-04-05 23:29:55 +0800

    modify

--- fyf_server.py
+++ fyf_server.py
@@ -7,9 +7,9 @@ fyf_socket.listen(100)
 
 while True:
     # http协议
-    # 连接, 客户端地址
+    # socket连接(对象), 客户端地址
     http_connect, address = fyf_socket.accept()
-    # 服务器接收到的请求报文recv(1024)
+    # 服务器接收到的请求报文 对象的recv(1024)方法读取请求报文
     print(http_connect.recv(1024), '\n', address, '\n')
     # 响应报文
     #http_connect.send(b'HTTP/1.1 200 OK\r\n\Content-Type:text/html; charset=utf-8\r\n\r\n')
