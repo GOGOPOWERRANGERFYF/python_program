fb5a6881c5376d69fd505b0a9080cb932a17e263
GOGOPOWERRANGERFYF <392138779@qq.com>
2021-04-05 21:17:46 +0800

    add

---
+++ fyf_server.py
@@ -0,0 +1,18 @@
+import socket
+
+# tcp/ip协议
+fyf_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+fyf_socket.bind(('127.0.0.1', 8800))
+fyf_socket.listen(100)
+
+while True:
+    # http协议
+    # 服务器接收到的请求报文
+    http_connect, address = fyf_socket.accept()
+    print(http_connect.recv(1024), address)
+    # 响应报文
+    #http_connect.send(b'HTTP/1.1 200 OK\r\n\Content-Type:text/html; charset=utf-8\r\n\r\n')
+    http_connect.send('HTTP/1.1 200 OK\r\n\Content-Type:text/html; charset=utf-8\r\n\r\n'.encode('utf-8'))
+    http_connect.send("<h1>Hello, This is fyf website!</h1>".encode('utf-8'))
+    http_connect.close()
+
