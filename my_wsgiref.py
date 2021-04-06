f586d28698f087a5387dd6438dc51c80dede6c20
GOGOPOWERRANGERFYF <392138779@qq.com>
2021-04-06 22:16:11 +0800

    modify

--- my_wsgiref.py
+++ my_wsgiref.py
@@ -1,2 +1,13 @@
 # WSGI web server gateway interface 
 # 
+# web应用 
+# web框架
+# web服务 
+# socket(套接字)
+# 
+# wsgiref模块功能: 
+#   按照http协议解析请求数据(报文)
+#   按照http协议封装响应数据(报文)
+#   本质就是处理字节串
+#   例如:将请求报文解析后赋值给一个字典dict={"path":"/xxx/xx",...,......}
+# 
